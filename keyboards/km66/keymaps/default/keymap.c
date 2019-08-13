ccdc2e19fce985b24db1c89035eda2f057b9bae5
Cyril Lugan <cyril@lugan.fr>
2019-08-13 19:45:53 +0200

    Caps Lock led

--- keyboards/km66/keymaps/default/keymap.c
+++ keyboards/km66/keymaps/default/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,   KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
     KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUHS, \
     KC_ESC,  KC_A,   KC_S,    KC_D,    KC_F,   KC_G,             KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
-    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,    KC_UP,   KC_NUBS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    MT(MOD_LSFT, KC_CAPS), KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,    KC_UP,   KC_NUBS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
     KC_LCTL,         KC_LALT, KC_LGUI, KC_SPC, KC_LEFT, KC_DOWN, KC_RIGHT, LT(1, KC_SPC),  KC_RALT, KC_RGUI,                   KC_RCTL \
   ),
   [1] = LAYOUT( /* Fn */
@@ -92,5 +92,14 @@ void matrix_scan_user(void) {
 }
 
 void led_set_user(uint8_t usb_led) {
-
+    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+        writePinHigh(F0);
+    } else {
+        writePinLow(F0);
+    }
+    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+        writePinHigh(F1);
+    } else {
+        writePinLow(F1);
+    }
 }
