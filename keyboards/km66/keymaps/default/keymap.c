333e9f25b3621706f18e1836468ebdb908b98a54
Cyril Lugan <cyril@lugan.fr>
2019-08-14 15:10:41 +0200

    Trying dead keys

--- keyboards/km66/keymaps/default/keymap.c
+++ keyboards/km66/keymaps/default/keymap.c
@@ -46,18 +46,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
   [0] = LAYOUT( /* Base */
-    KC_GRV,  KC_1,   KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-    KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUHS, \
-    KC_ESC,  KC_A,   KC_S,    KC_D,    KC_F,   KC_G,             KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
-    MT(MOD_LSFT, KC_CAPS), KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,    KC_UP,   KC_NUBS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL,         KC_LALT, KC_LGUI, KC_SPC, KC_LEFT, KC_DOWN, KC_RIGHT, LT(1, KC_SPC),  KC_RALT, KC_RGUI,                   KC_RCTL \
+    KC_GRV,        KC_1,   KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+    KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUHS, \
+    KC_ESC,        KC_A,   KC_S,    KC_D,    KC_F,   KC_G,             KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
+    OSM(MOD_LSFT), KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,    KC_UP,   KC_NUBS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT), \
+    KC_LCTL,               KC_LALT, KC_LGUI, KC_SPC, KC_LEFT, KC_DOWN, KC_RIGHT, OSL(1),  OSM(MOD_RALT), KC_RGUI,                   KC_RCTL \
   ),
   [1] = LAYOUT( /* Fn */
-    ____, KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DELETE, \
-    ____, HYPR(KC_Q), HYPR(KC_W), HYPR(KC_E), HYPR(KC_R), HYPR(KC_T), KC_HOME, KC_PGDN, KC_PGUP, KC_END,    ____,    OSX_TAB_PREV, OSX_TAB_NEXT, ____, \
-    ____, HYPR(KC_A), HYPR(KC_S), HYPR(KC_D), HYPR(KC_F), HYPR(KC_G),             KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,    ____, ____, ____, \
-    ____, HYPR(KC_Z), HYPR(KC_X), HYPR(KC_C), HYPR(KC_V), HYPR(KC_B), KC_UP,   KC_NUBS,  OSX_DEL_LINE, OSX_DEL_WORD, OSX_FDEL_WORD, OSX_FDEL_LINE, ____, ____, \
-    ____,             ____,       ____,       ____,       ____,       ____,    ____,     ____,         ____,         ____,                               ____ \
+    KC_PWR,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DELETE, \
+    ____,    HYPR(KC_Q), HYPR(KC_W), HYPR(KC_E), HYPR(KC_R), HYPR(KC_T), KC_HOME, KC_PGDN, KC_PGUP, KC_END,    ____,    OSX_TAB_PREV, OSX_TAB_NEXT, ____, \
+    ____,    HYPR(KC_A), HYPR(KC_S), HYPR(KC_D), HYPR(KC_F), HYPR(KC_G),             KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,    ____, ____, ____, \
+    KC_CAPS, HYPR(KC_Z), HYPR(KC_X), HYPR(KC_C), HYPR(KC_V), HYPR(KC_B), KC_UP,   KC_NUBS,  OSX_DEL_LINE, OSX_DEL_WORD, OSX_FDEL_WORD, OSX_FDEL_LINE, ____, KC_NLCK, \
+    ____,                ____,       ____,       ____,       ____,       ____,    ____,     ____,         ____,         ____,                               ____ \
   )
 };
 
